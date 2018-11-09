688fec19808018e6f572dae266868baabc129175
Wgw <707612123@qq.com>
2018-11-09 16:13:57 +0800

    init

--- src/main/java/com/funtl/itoken/eureka/EurekaApplication.java
+++ src/main/java/com/funtl/itoken/eureka/EurekaApplication.java
@@ -4,18 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * <p>Title: EurekaApplication</p>
- * <p>Description: </p>
- *
- * @author wgw
- * @version 1.0.0
- * @date 2018/11/9 15:30
- */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class,args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
